965bd3e3a59ab54f290a4377a82749730baa1553
Renan Cesar <producer@skynance.net>
2022-09-01 00:46:43 -0300

    :sparkles: feat(clean): clean project

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,26 +1,24 @@
-export const defaultTheme = {
-  white: '#fff',
+import { createGlobalStyle } from 'styled-components'
 
-  'gray-100': '#E1E1E6',
-  'gray-300': '#C4C4CC',
-  'gray-400': '#8D8D99',
-  'gray-500': '#7C7C8A',
-  'gray-600': '#323238',
-  'gray-700': '#29292E',
-  'gray-800': '#202024',
-  'gray-900': '#121214',
+export const GlobalStyle = createGlobalStyle`
+  * {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  }
 
-  'blue-300': '#81d8f7',
-  'blue-400': '#5bc0de',
-  'blue-500': '#2196F3',
-  'blue-600': '#0c7cd5',
-  'blue-700': '#0a539e',
+  :focus {
+    outline: none;
+    box-shadow: 0 0 0 2px ${(props) => props.theme['green-500']};
+  }
 
-  'green-300': '#00B37E',
-  'green-500': '#00875F',
-  'green-700': '#015F43',
+  body {
+    background-color: ${(props) => props.theme['gray-800']};
+    color: ${(props) => props.theme['gray-100']};
+    -webkit-font-smoothing: antialiased;
+  }
 
-  'red-300': '#F75A68',
-  'red-500': '#AB222E',
-  'red-700': '#7A1921'
-} as const
+  body, input, textarea, button {
+    font: 400 1rem Roboto, sans-serif;
+  }
+`
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,26 @@
+export const defaultTheme = {
+  white: '#fff',
+
+  'gray-100': '#E1E1E6',
+  'gray-300': '#C4C4CC',
+  'gray-400': '#8D8D99',
+  'gray-500': '#7C7C8A',
+  'gray-600': '#323238',
+  'gray-700': '#29292E',
+  'gray-800': '#202024',
+  'gray-900': '#121214',
+
+  'blue-300': '#81d8f7',
+  'blue-400': '#5bc0de',
+  'blue-500': '#2196F3',
+  'blue-600': '#0c7cd5',
+  'blue-700': '#0a539e',
+
+  'green-300': '#00B37E',
+  'green-500': '#00875F',
+  'green-700': '#015F43',
+
+  'red-300': '#F75A68',
+  'red-500': '#AB222E',
+  'red-700': '#7A1921'
+} as const
